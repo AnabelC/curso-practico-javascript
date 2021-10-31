82a885ac9fb34e75fa592fc9e26de8b7c997699c
Anabel Campagnoli <anitacampagnoli126@hotmail.com>
2021-10-31 14:22:27 -0400

    Agregar moda y reto clase 18

--- mediana.js
+++ mediana.js
@@ -1,8 +1,12 @@
 const lista1 = [
-    100,200,500,400000000,
+    500,800,100,200,400000000,
 ];
 
-let mitadLista1 = parseInt(lista1.length/2);
+const listaOrdenada = lista1.sort(function(a, b) {
+    return a - b;
+    });
+
+let mitadListaOrdenada = parseInt(listaOrdenada.length/2);
 
 function esPar(numerito){
     if(numerito % 2 === 0){
@@ -24,11 +28,11 @@ function calcularMediaAritmetica(lista) {
 
 let mediana;
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 =  lista1[mitadLista1];
+if (esPar(listaOrdenada.length)) {
+    const elemento1 = listaOrdenada[mitadListaOrdenada - 1];
+    const elemento2 =  listaOrdenada[mitadListaOrdenada];
     const promedioElemento1y2 = calcularMediaAritmetica([elemento1,elemento2]);
     mediana = promedioElemento1y2;
 } else {
-    mediana = lista1[mitadLista1];
+    mediana = listaOrdenada[mitadListaOrdenada];
 }
